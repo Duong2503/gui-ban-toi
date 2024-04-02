0fd1ff0cd68b16e8722ad1e1602a15e9b224245c
Duong2503 <86710440+Duong2503@users.noreply.github.com>
2024-04-02 20:27:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Bạn tôi à!',
+    introTitle: 'cuns oi...',
     introDesc: `Trái đất vốn lạ thường
     Mà sao bạn cứ đi nhầm đường
-    Lạc vào tim tôi lẻ loi
+    Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng bạn đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng bạn đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích tôi <33',
-    btnNo: 'Không, bạn tôi à :3',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là tha lỗi cho tuii <33',
+    btnNo: 'Không c:',
     question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu tôi <3',
     btnReply: 'Gửi cho tôi <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'tôi biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Xem video tôi cất công làm cho bạn nhá :3.',
+    mess: 'anh biết mà 🥰. Yêu emm nhiều nhiều 😘😘',
+    messDesc: 'nếu vẫn chưa đủ thuyết phục thì xem ảnh này nhéeee.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 }
